b445ade55867dda131648ff89c8ba021ebcf9723
bibaocodekopro <95998397+bibaocodekopro@users.noreply.github.com>
2023-02-15 15:47:45 +0700

    first day

---
+++ src/route/web.js
@@ -0,0 +1,11 @@
+import express from "express";
+import homeCotroller from "../controller/homeCotroller";
+
+let router = express.Router();
+
+let initWebRouters= (app) =>{
+    router.get('/',homeCotroller.getHomePage);
+    return  app.use("/",router);
+}
+
+module.exports= initWebRouters;
