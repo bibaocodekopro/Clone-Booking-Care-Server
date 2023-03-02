b347b25986225da6a9d8f7486cb1a99211de0261
bibaocodekopro <95998397+bibaocodekopro@users.noreply.github.com>
2023-03-02 14:27:17 +0700

    learn about CRUD with ReactJS part1 (server)

--- src/controller/userController.js
+++ src/controller/userController.js
@@ -17,6 +17,50 @@ let handleLogin = async(req,res)=>{
         user: userData.user ? userData.user :{},
     })
 }
+
+let handleGetAllUser = async(req,res)   => {
+    let id = req.query.id; //ALL, ID
+    if(!id){
+        return res.status(200).json({
+        errCode: 1,
+        errMessage: 'Missing input parameter',
+        users: []
+    })
+    }
+    let users= await userServices.getAllUser(id);
+    console.log(users);
+    return res.status(200).json({
+        errCode: 0,
+        errMessage: 'OK',
+        users
+    })
+}
+
+let handleCreateNewUser = async(req,res) =>{
+    let message = await userServices.createNewUser(req.body);
+    console.log(message);
+    return res.status(200).json(message);
+}
+let handleDeleteUser = async(req,res)=>{
+    if(!req.body.id){
+        return res.status(200).json({
+            errCode:1,
+            errMessage:'Missing required parameters!'
+        }) 
+    }
+    let message = await userServices.deleteUser(req.body.id);
+    return res.status(200).json(message);
+}
+let handleEditUser=async (req,res)=>{
+    let data= req.body;
+    let message = await userServices.updateUser(data);
+    return res.status(200).json(message);
+}
 module.exports = {
     handleLogin: handleLogin,
+    handleGetAllUser:  handleGetAllUser,
+    handleCreateNewUser: handleCreateNewUser,
+    handleEditUser: handleEditUser,
+    handleDeleteUser: handleDeleteUser,
+
 }
